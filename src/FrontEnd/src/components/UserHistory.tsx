1e64b4b06e1789de4fee1a5aad3a4147611620e0
Vishawjeet sidhu <81245123+lsvishawjeet@users.noreply.github.com>
2024-09-26 17:33:40 +0530

    Added loading in history

--- src/FrontEnd/src/components/UserHistory.tsx
+++ src/FrontEnd/src/components/UserHistory.tsx
@@ -6,10 +6,12 @@ import axios from 'axios'
 function UserHistory() {
     const { user } = useUser()
     const [historyData, setHistoryData] = useState<any>([])
+    const [loading, setLoading] = useState<boolean>(true) // Track loading state
 
     useEffect(() => {
         async function getData() {
             try {
+                setLoading(true) // Start loading
                 const userId = user?.publicMetadata?.userId
                 console.log(userId)
                 const { data } = await axios.get(`/api/getHistory/${userId}`)
@@ -17,6 +19,8 @@ function UserHistory() {
                 setHistoryData(data)
             } catch (error) {
                 console.log("Error fetching history", error)
+            } finally {
+                setLoading(false) // End loading
             }
         }
         if (user?.publicMetadata?.userId) {
@@ -25,9 +29,20 @@ function UserHistory() {
     }, [user])
 
     return (
-        <div className=" min-h-screen">
+        <div className="min-h-screen">
             <h1 className="text-lg font-bold mb-4">Prediction History</h1>
-            {historyData.length > 0 ? (
+            {loading ? (
+                <div className="flex justify-center items-center h-full">
+                    {/* Improved Loader */}
+                    <div className="relative">
+                        <div className="w-16 h-16 rounded-full border-t-4 border-gray-500 border-b-4  animate-spin"></div>
+                        <div className="absolute inset-0 flex items-center justify-center">
+                            {/* <div className="w-10 h-10  bg-gray-600 rounded-full"></div> */}
+                        </div>
+                    </div>
+                    <p className="ml-4 text-gray-600 text-lg">Loading...</p> 
+                </div>
+            ) : historyData.length > 0 ? (
                 historyData.map((item: any, index: number) => (
                     <div key={index} className="bg-white shadow-md rounded-lg p-4 mb-4">
                         <div className="flex flex-col gap-2 mb-4">
@@ -49,7 +64,7 @@ function UserHistory() {
                             </div>
                         </div>
                         <div className="border-t border-gray-300 pt-2 flex">
-                            <p><span  className="text-lg font-semibold">Predicted Crop: </span><span className="text-lg text-gray-800">{item.predictedCrop}</span> </p>
+                            <p><span className="text-lg font-semibold">Predicted Crop: </span><span className="text-lg text-gray-800">{item.predictedCrop}</span> </p>
                         </div>
                     </div>
                 ))
